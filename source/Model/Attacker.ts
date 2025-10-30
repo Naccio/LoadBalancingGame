780f2d0274dc0b600b852a59cb0f9c6de2f5bc49
Naccio <naccio.lallalla@gmail.com>
2025-10-30 12:47:44 +0100

    Spell fixes pt.2

--- source/Model/Attacker.ts
+++ source/Model/Attacker.ts
@@ -6,13 +6,13 @@ class Attacker implements MessageTransmitter {
     public messagesToSend: number;
     public messagesToReceive: number;
 
-    constructor(private orchestrator: MessageOrchestrator, public x: number, public y: number, public msgNr: number, public connectedTo: Server) {
+    constructor(private orchestrator: MessageOrchestrator, public x: number, public y: number, public messages: number, public connectedTo: Server) {
         this.x = x;
         this.y = y;
         this.connectedTo = connectedTo;
         this.lastMessageTime = 0;
-        this.messagesToSend = msgNr;
-        this.messagesToReceive = msgNr;
+        this.messagesToSend = messages;
+        this.messagesToReceive = messages;
     }
 
     sendMessage(elapsedTime: number) {
